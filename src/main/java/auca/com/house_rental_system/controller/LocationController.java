98f082a60878747da4ef207921d08b914d5c12b6
Epaphras <ishimweepa18@gmail.com>
2026-03-13 16:53:51 +0200

    code refactoring

--- src/main/java/auca/com/house_rental_system/controller/LocationController.java
+++ src/main/java/auca/com/house_rental_system/controller/LocationController.java
@@ -2,18 +2,9 @@ package auca.com.house_rental_system.controller;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import auca.com.house_rental_system.model.Location;
 import auca.com.house_rental_system.service.LocationService;
